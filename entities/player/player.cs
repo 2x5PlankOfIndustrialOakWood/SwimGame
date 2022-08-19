d64daebc02d91c9dd2bab86c943eb443ee7bc5e2
2x5PlankOfIndustrialOakWood <engooeer@gmail.com>
2022-08-19 13:20:09 -0700

    added movement

--- entities/player/player.cs
+++ entities/player/player.cs
@@ -7,33 +7,38 @@ public class player : KinematicBody2D
     [Export] public int decaySpeed;
     [Export] public int rotSpeed;
 
+    public int rotationDir = 0;
+
     public Vector2 velocity = new Vector2();
 
-    public void GetInput()
+    public void GetInput(float delta)
     {
         velocity = new Vector2();
+        
+        rotationDir = 0;
 
         if (Input.IsActionPressed("ui_up"))
         {
-            velocity.y -= 1;
+            velocity = new Vector2(0, -speed).Rotated(Rotation);
+            
         }
 
-        if (Input.IsActionPressed("ui_down"))
-        {
-            velocity.y += 1;
-        }
+        //if (Input.IsActionPressed("ui_down"))
+        //{
+        //    velocity.y += 1;
+        //}
 
         if (Input.IsActionPressed("ui_left"))
         {
-            GD.Print("left");
+            rotationDir -= 1;
         }
 
         if (Input.IsActionPressed("ui_right"))
         {
-            GD.Print("right");
+            rotationDir += 1;
         }
 
-        velocity = velocity.Normalized() * speed;
+        velocity = velocity.Normalized() * delta * speed;
     }
     public override void _Ready()
     {
@@ -42,7 +47,19 @@ public class player : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        GetInput();
+        AnimatedSprite animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        GetInput(delta);
+        Rotation += rotationDir * rotSpeed * delta;
         velocity = MoveAndSlide(velocity);
+
+        if (velocity.Length() > 0)
+        {
+            animatedSprite.Play("flying");
+        }
+        else
+        {
+            animatedSprite.Set("flying", 0);
+            animatedSprite.Stop();
+        }
     }
 }
